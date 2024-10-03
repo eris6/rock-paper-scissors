033cf03e051f539e89ec09b9417449d8923a422f
eris6 <erispatel20@gmail.com>
2024-10-03 14:00:28 -0400

    Add running score display
    Game now displays human and computer score underneath

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -6,33 +6,35 @@ function getComputerChoice(){
     return (randomNum >= 0  && randomNum < 34) ? "rock" : (randomNum > 67 ) ? "paper" : "scissors";
 }
 
-function getHumanChoice(){
-    let choice = prompt("Enter rock, paper, or scissors");
-    choice = choice.toLowerCase();
+// function getHumanChoice(){
+//     let choice = prompt("Enter rock, paper, or scissors");
+//     choice = choice.toLowerCase();
 
-    if (choice == "rock"){
-        return "rock";
-    }
-    else if (choice == "paper"){
-        return "paper";
-    }
-    else if (choice == "scissors"){
-        return "scissors";
-    }
-    else{
-        return "invalid input!";
-    }
-}
+//     if (choice == "rock"){
+//         return "rock";
+//     }
+//     else if (choice == "paper"){
+//         return "paper";
+//     }
+//     else if (choice == "scissors"){
+//         return "scissors";
+//     }
+//     else{
+//         return "invalid input!";
+//     }
+// }
 
 function playRound(humanChoice, computerChoice){
 
     const result = document.querySelector(".result");
-    
-
+    const humandisplay = document.querySelector(".humanscore");
+    const computerdisplay = document.querySelector(".computerscore");
 
     if (humanChoice == computerChoice){
         console.log("Tie game! There is no winner or loser.");
         result.innerHTML = "Tie game! There is no winner or loser.";
+        humandisplay.innerHTML = "You  " + humanScore;
+        computerdisplay.innerHTML = "Computer " + computerScore;
     }
     
     else{
@@ -44,7 +46,11 @@ function playRound(humanChoice, computerChoice){
         humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
         computerChoice = computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
         console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
+        console.log(humanScore);
+        console.log(computerScore);
         result.innerHTML = "You win! " + humanChoice + " beats " + computerChoice.toLowerCase() + ".";
+        humandisplay.innerHTML = "You " + humanScore;
+        computerdisplay.innerHTML = "Computer " + computerScore;
     }
     else{
         humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
@@ -52,31 +58,33 @@ function playRound(humanChoice, computerChoice){
 
         computerScore += 1;
         console.log("You lose! " + computerChoice + " beats " + humanChoice + ".");
+        console.log(humanScore);
+        console.log(computerScore);
         result.innerHTML = "You lose! " + computerChoice + " beats " + humanChoice.toLowerCase()  + ".";
+        humandisplay.innerHTML = "You " + humanScore;
+        computerdisplay.innerHTML = "Computer " + computerScore;
     }
     }
 }
 
-// function playGame(){
-//     let numGames = 0;
-    
+function playGame(){
+    let numGames = 0;
 
-//     while (numGames < 5){
+    while (numGames < 5){
+
+        playRound(humanSelection, computerSelection);
         
-//         let computerSelection = getComputerChoice();
-//         let humanSelection = getHumanChoice();
-
-//         if (humanSelection == "invalid input!"){
-//             continue;
-//         }
-//         else{
-//             playRound(humanSelection, computerSelection);
-//             numGames++;
-//         }
+        if (humanSelection == "invalid input!"){
+            continue;
+        }
+        else{
+            playRound(humanSelection, computerSelection);
+            numGames++;
+        }
 
         
-//     }
-// }
+    }
+}
 
 
 
@@ -103,3 +111,6 @@ window.onload = function(){
     })
 
 };
+
+
+playGame();
